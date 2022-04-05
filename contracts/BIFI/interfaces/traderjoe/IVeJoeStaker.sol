4ee98038aea9f7d2a4179c32ee8fddd10e026ecb
Eric Cartman <66183913+cartman-dev@users.noreply.github.com>
2022-04-05 10:28:11 -0400

    StrategyTraderJoeBoostedLP + VeJoeStaker

---
+++ contracts/BIFI/interfaces/traderjoe/IVeJoeStaker.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+interface IVeJoeStaker {
+    function deposit(uint256 _pid, uint256 _amount) external;
+    function withdraw(uint256 _pid, uint256 _amount) external;
+    function emergencyWithdraw(uint256 _pid) external;
+}

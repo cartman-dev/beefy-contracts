a43a750242f39d4908f6b2abb579ddcdb2fa31f7
Eric Cartman <66183913+cartman-dev@users.noreply.github.com>
2023-02-16 09:38:10 -0500

    Add StrategyCommonMiniChefMultiRewardsLP for Pangolin

---
+++ test/prod/minichefVaultLifecycle.test.js
@@ -0,0 +1,221 @@
+const { expect } = require("chai");
+import { addressBook } from "blockchain-addressbook";
+import { chainCallFeeMap } from "../../utils/chainCallFeeMap";
+
+const { zapNativeToToken, getVaultWant, unpauseIfPaused, getUnirouterData } = require("../../utils/testHelpers");
+const { delay } = require("../../utils/timeHelpers");
+
+const TIMEOUT = 10 * 60 * 1000000;
+
+const chainName = "avax";
+const chainData = addressBook[chainName];
+const { beefyfinance } = chainData.platforms;
+
+const config = {
+  vault: "0x59d989e922BE99Ea344DDa647a5a2c15045c9025",
+  vaultContract: "BeefyVaultV7",
+  strategyContract: "StrategyCommonMiniChefMultiRewardsLP",
+  testAmount: ethers.utils.parseEther("5"),
+  wnative: chainData.tokens.WNATIVE.address,
+  keeper: beefyfinance.keeper,
+  strategyOwner: beefyfinance.strategyOwner,
+  vaultOwner: beefyfinance.vaultOwner,
+};
+
+describe("minichefVaultLifecycleTest", () => {
+  let vault, strategy, unirouter, want, deployer, keeper, other;
+
+  beforeEach(async () => {
+    [deployer, keeper, other] = await ethers.getSigners();
+
+    vault = await ethers.getContractAt(config.vaultContract, config.vault);
+    const strategyAddr = await vault.strategy();
+    strategy = await ethers.getContractAt(config.strategyContract, strategyAddr);
+
+    const unirouterAddr = await strategy.unirouter();
+    const unirouterData = getUnirouterData(unirouterAddr);
+    unirouter = await ethers.getContractAt(unirouterData.interface, unirouterAddr);
+    want = await getVaultWant(vault, config.wnative);
+
+    await zapNativeToToken({
+      amount: config.testAmount,
+      want,
+      nativeTokenAddr: config.wnative,
+      unirouter,
+      swapSignature: unirouterData.swapSignature,
+      recipient: deployer.address,
+    });
+    const wantBal = await want.balanceOf(deployer.address);
+    await want.transfer(other.address, wantBal.div(2));
+    await unpauseIfPaused(strategy, deployer);
+  });
+
+  it("User can deposit and withdraw from the vault.", async () => {
+    const wantBalStart = await want.balanceOf(deployer.address);
+
+    await want.approve(vault.address, wantBalStart);
+    await vault.depositAll();
+    await vault.withdrawAll();
+
+    const wantBalFinal = await want.balanceOf(deployer.address);
+
+    expect(wantBalFinal).to.be.lte(wantBalStart);
+    expect(wantBalFinal).to.be.gt(wantBalStart.mul(99).div(100));
+  }).timeout(TIMEOUT);
+
+  it("Harvests work as expected.", async () => {
+    const wantBalStart = await want.balanceOf(deployer.address);
+    await want.approve(vault.address, wantBalStart);
+    await vault.depositAll();
+
+    const vaultBal = await vault.balance();
+    const pricePerShare = await vault.getPricePerFullShare();
+    // await delay(5000);
+    await network.provider.send("evm_increaseTime", [60 * 60 * 24]); // 1 hour
+    await network.provider.send("evm_mine");
+    const callRewardBeforeHarvest = await strategy.callReward();
+    expect(callRewardBeforeHarvest).to.be.gt(0);
+    await strategy.managerHarvest();
+    const vaultBalAfterHarvest = await vault.balance();
+    const pricePerShareAfterHarvest = await vault.getPricePerFullShare();
+    // const callRewardAfterHarvest = await strategy.callReward();
+
+    await vault.withdrawAll();
+    const wantBalFinal = await want.balanceOf(deployer.address);
+
+    expect(vaultBalAfterHarvest).to.be.gt(vaultBal);
+    expect(pricePerShareAfterHarvest).to.be.gt(pricePerShare);
+    // expect(callRewardBeforeHarvest).to.be.gt(callRewardAfterHarvest);
+
+    expect(wantBalFinal).to.be.gt(wantBalStart.mul(99).div(100));
+
+    const lastHarvest = await strategy.lastHarvest();
+    expect(lastHarvest).to.be.gt(0);
+  }).timeout(TIMEOUT);
+
+  it("Manager can panic.", async () => {
+    const wantBalStart = await want.balanceOf(deployer.address);
+    await want.approve(vault.address, wantBalStart);
+    await vault.depositAll();
+
+    const vaultBal = await vault.balance();
+    const balOfPool = await strategy.balanceOfPool();
+    const balOfWant = await strategy.balanceOfWant();
+    await strategy.panic();
+    const vaultBalAfterPanic = await vault.balance();
+    const balOfPoolAfterPanic = await strategy.balanceOfPool();
+    const balOfWantAfterPanic = await strategy.balanceOfWant();
+
+    expect(vaultBalAfterPanic).to.be.gt(vaultBal.mul(99).div(100));
+    expect(balOfPool).to.be.gt(balOfWant);
+    expect(balOfWantAfterPanic).to.be.gt(balOfPoolAfterPanic);
+
+    // Users can't deposit.
+    const tx = vault.depositAll();
+    await expect(tx).to.be.revertedWith("Pausable: paused");
+
+    // User can still withdraw
+    await vault.withdrawAll();
+    const wantBalFinal = await want.balanceOf(deployer.address);
+    expect(wantBalFinal).to.be.gt(wantBalStart.mul(99).div(100));
+  }).timeout(TIMEOUT);
+
+  it("New user deposit/withdrawals don't lower other users balances.", async () => {
+    const wantBalStart = await want.balanceOf(deployer.address);
+    await want.approve(vault.address, wantBalStart);
+    await vault.depositAll();
+
+    const pricePerShare = await vault.getPricePerFullShare();
+    const wantBalOfOther = await want.balanceOf(other.address);
+    await want.connect(other).approve(vault.address, wantBalOfOther);
+    await vault.connect(other).depositAll();
+    const pricePerShareAfterOtherDeposit = await vault.getPricePerFullShare();
+
+    await vault.withdrawAll();
+    const wantBalFinal = await want.balanceOf(deployer.address);
+    const pricePerShareAfterWithdraw = await vault.getPricePerFullShare();
+
+    expect(pricePerShareAfterOtherDeposit).to.be.gte(pricePerShare);
+    expect(pricePerShareAfterWithdraw).to.be.gte(pricePerShareAfterOtherDeposit);
+    expect(wantBalFinal).to.be.gt(wantBalStart.mul(99).div(100));
+  }).timeout(TIMEOUT);
+
+  it.skip("It has the correct owners and keeper.", async () => {
+    const vaultOwner = await vault.owner();
+    const stratOwner = await strategy.owner();
+    const stratKeeper = await strategy.keeper();
+
+    expect(vaultOwner).to.equal(config.vaultOwner);
+    expect(stratOwner).to.equal(config.strategyOwner);
+    expect(stratKeeper).to.equal(config.keeper);
+  }).timeout(TIMEOUT);
+
+  it("Vault and strat references are correct", async () => {
+    const stratReference = await vault.strategy();
+    const vaultReference = await strategy.vault();
+
+    expect(stratReference).to.equal(ethers.utils.getAddress(strategy.address));
+    expect(vaultReference).to.equal(ethers.utils.getAddress(vault.address));
+  }).timeout(TIMEOUT);
+
+  it.skip("Displays routing correctly", async () => {
+    const { tokenAddressMap } = addressBook[chainName];
+
+    console.log("nativeToLp0Route:");
+    for (let i = 0; i < 10; ++i) {
+      try {
+        const tokenAddress = await strategy.nativeToLp0Route(i);
+        if (tokenAddress in tokenAddressMap) {
+          console.log(tokenAddressMap[tokenAddress].symbol);
+        } else {
+          console.log(tokenAddress);
+        }
+      } catch {
+        // reached end
+        if (i == 0) {
+          console.log("No routing, output must be lp0");
+        }
+        break;
+      }
+    }
+
+    console.log("nativeToLp1Route:");
+    for (let i = 0; i < 10; ++i) {
+      try {
+        const tokenAddress = await strategy.nativeToLp1Route(i);
+        if (tokenAddress in tokenAddressMap) {
+          console.log(tokenAddressMap[tokenAddress].symbol);
+        } else {
+          console.log(tokenAddress);
+        }
+      } catch {
+        // reached end
+        if (i == 0) {
+          console.log("No routing, output must be lp1");
+        }
+        break;
+      }
+    }
+  }).timeout(TIMEOUT);
+
+  it.skip("Has correct call fee", async () => {
+    const callFee = await strategy.callFee();
+
+    const expectedCallFee = chainCallFeeMap[chainName];
+    const actualCallFee = parseInt(callFee);
+
+    expect(actualCallFee).to.equal(expectedCallFee);
+  }).timeout(TIMEOUT);
+
+  it.skip("has withdraw fee of 0 if harvest on deposit is true", async () => {
+    const harvestOnDeposit = await strategy.harvestOnDeposit();
+
+    const withdrawalFee = await strategy.withdrawalFee();
+    const actualWithdrawalFee = parseInt(withdrawalFee);
+    if (harvestOnDeposit) {
+      expect(actualWithdrawalFee).to.equal(0);
+    } else {
+      expect(actualWithdrawalFee).not.to.equal(0);
+    }
+  }).timeout(TIMEOUT);
+});
